002467ecb03f171dd172b1c410535158e9f07b4b
Christopher B <cballenger@steamsport.com>
2019-05-29 22:15:16 -0400

    DuckDuckGo was added.

--- start-page/engines/base.js
+++ start-page/engines/base.js
@@ -47,5 +47,16 @@ eng.youtube = {
 	logo: "youtube.png",
 	places: {
 		'Videos' : ["http://www.youtube.com/results?search_query=%query%", apiUrl+"?eng=youtube&timestamp=%time%&q=%query%"]
-	},
+	}
+};
+
+/*	DUCKDUCKGO	
+	----------------------------------------------------- */
+
+eng.duckduckgo = {
+	pageTitle: "DuckDuckGo",
+	logo: "duckduckgo.png",
+	places: {
+		'Search DuckDuckGo' : ["https://www.ddg.co/?q=%query%", true]
+	}
 };
